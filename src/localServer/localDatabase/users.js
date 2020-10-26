7c3893e313ce8c027949234724a89a2653c50494
Ербол Сыздық <ysyzdyk@alfabank.kz>
2020-10-27 05:42:30 +0600

    fix(i18n): add internationalization on users db

--- src/localServer/localDatabase/users.js
+++ src/localServer/localDatabase/users.js
@@ -2,9 +2,25 @@ import getFromLocalStorage from '../../helpers/getFromLocalStorage.js'
 import generateId from '../../helpers/generateId.js'
 import saveToLocalStorage from '../../helpers/saveToLocalStorage.js'
 import avatars from './avatars.js'
+import i18next from 'i18next'
 
 const lsName = 'users'
 
+i18next.addResourceBundle('en', 'usersLocalDb', {
+  'This name is taken': 'This name is taken',
+  'Wrong login or password': 'Wrong login or password',
+  'User not found': 'User not found',
+});
+i18next.addResourceBundle('ru', 'usersLocalDb', {
+  'This name is taken': 'Этот логин уже используется',
+  'Wrong login or password': 'Введен неверный логин или пароль',
+  'User not found': 'Пользователь не найден',
+});
+i18next.addResourceBundle('kk', 'usersLocalDb', {
+  'This name is taken': 'Бұл пайдаланушы аты қолданыста',
+  'Wrong login or password': 'Пайдаланушы аты немесе құпия сөз қате енгізілген',
+  'User not found': 'Пайдаланушы табылмады',
+});
 
 
 export function addUser(user) {
@@ -17,7 +33,7 @@ export function addUser(user) {
       avatarIndex: Math.floor(Math.random() * avatars.length),
     }
     if (users.some((u) => u.name === user.name)) {
-      rej(new Error('this name is taken'))
+      rej(new Error(i18next.t('usersLocalDb:This name is taken')))
       return
     }
     users.push(newUser)
@@ -42,7 +58,7 @@ export function getUserIdByCredentials(name, password) {
       res(user.id)
       return
     }
-    rej(new Error('Введен неверный логин или пароль'))
+    rej(new Error(i18next.t('usersLocalDb:Wrong login or password')))
   })
 }
 
@@ -63,6 +79,6 @@ export function getUserById(id) {
       res(normalizeUser(user))
       return
     }
-    rej(new Error('Пользователь не найден'))
+    rej(new Error(i18next.t('usersLocalDb:User not found')))
   })
 }
